6c966e060b5f57a130a995befc66335a069d0765
Kulikowski Konrad <kulikowski.konrad93@gmail.com>
2021-10-10 19:15:02 +0200

    Add shopping list entities

---
+++ src/SplitCost.Domain/ShoppingList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitCost.Domain
+{
+    public class ShoppingList
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime ModificationDate { get; private set; }
+        public List<ShoppingListItem> Items { get; private set; }
+
+        private ShoppingList()
+        {
+            // For EF
+        }
+
+        public ShoppingList(Guid id, string name, List<ShoppingListItem> items = null)
+        {
+            Id = id;
+            Items = items ?? new List<ShoppingListItem>();
+            CreationDate = DateTime.UtcNow;
+            ModificationDate = DateTime.UtcNow;
+            SetName(name);
+        }
+
+        public void ChangeName(string name)
+        {
+            SetName(name);
+            ModificationDate = DateTime.UtcNow;
+        }
+
+        private void SetName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException($"'{nameof(name)}' cannot be null or empty", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public int Count()
+        {
+            if (Items is null)
+                throw new Exception("Property not included.");
+
+            return Items.Count;
+        }
+    }
+}
---
+++ src/SplitCost.Domain/ShoppingListItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SplitCost.Domain
+{
+    public class ShoppingListItem
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime ModificationDate { get; private set; }
+        public ShoppingList ShoppingList { get; private set; }
+        public Guid ShoppingListId { get; private set; }
+
+        public ShoppingListItem(Guid id, string name, ShoppingList shoppingList)
+        {
+            Id = id;
+            Name = name;
+            ShoppingList = shoppingList;
+            CreationDate = DateTime.UtcNow;
+            ModificationDate = DateTime.UtcNow;
+            ShoppingListId = shoppingList.Id;
+        }
+    }
+}
